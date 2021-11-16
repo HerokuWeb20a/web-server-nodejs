f67366b5b12b7744506aeb0f0e185fee96ee10af
Supr0en <erik.ruotsalainen1@gmail.com>
2021-11-16 11:12:02 +0200

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const path = require('path');
 
-const { host, port, sivut } = require('./config.json');
+const { host, port, sivut, varastokansio } = require('./config.json');
+const { lueVarasto } = require('./jsonvarasto/varastokasittelija');
 
 const kirjastopolku = path.join(__dirname, 'kirjasto');
 const { lue } = require(path.join(kirjastopolku, 'tiedostokasittelija'));
-const { laheta, onJoukossa } = require(path.join(kirjastopolku, 'resurssilahettaja'));
+const { laheta, onJoukossa, lahetaJson } = require(path.join(kirjastopolku, 'resurssilahettaja'));
 
 const resurssiReitit = ['/tyylit/','/js/','/img/'];
 
@@ -14,10 +15,16 @@ const valikkoPolku = path.join(sivutkansio, sivut.valikko);
 const paneeliPolku = path.join(sivutkansio, sivut.paneeli);
 const piirustusPolku = path.join(sivutkansio, sivut.piirustus);
 
+
 const server = http.createServer(async (req, res) => {
     const { pathname } = new URL(`http://${host}:${port}${req.url}`);
     const reitti = decodeURIComponent(pathname);
     const metodi = req.method.toUpperCase();
+
+    const Tietovarasto = 
+    require(path.join(__dirname,varastokansio,'tietovarastokerros'));
+
+    const varasto=new Tietovarasto();
 
     if (metodi === 'GET') {
         try {
@@ -25,6 +32,7 @@ const server = http.createServer(async (req, res) => {
                 laheta(res, await lue(valikkoPolku), 200);
             } else if (reitti === '/paneeli') {
                 laheta(res, await lue(paneeliPolku), 200);
+                lahetaJson(res, await varasto.haeKaikki());
             } else if (reitti === '/piirustus') {
                 laheta(res, await lue(piirustusPolku), 200);
             } else if (onJoukossa(reitti, ...resurssiReitit)) {
